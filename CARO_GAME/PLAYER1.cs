7410c123309d5b589bbd5da3316d1cd8dcef9bfd
Đinh Huỳnh Gia Bảo <100998772+andreafletcherdinh@users.noreply.github.com>
2024-05-29 22:50:01 +0700

    Update process bar's value, create a function for process bar tick and calls event
    Call events to mark player's turn and announce when game is end

--- CARO_GAME/PLAYER1.cs
+++ CARO_GAME/PLAYER1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,16 +13,57 @@ namespace CARO_GAME
     public partial class player1Form : Form
     {
         ChessBoardManager ChessBoard;
-        SettingControl settingControl;
         public player1Form()
         {
             InitializeComponent();
 
             ChessBoard = new ChessBoardManager(chessBoardPanel, nameTextBox, player1Image);
+            
+            // Gọi event khi kết thúc lượt của người chơi
+            ChessBoard.GameIsEnd += ChessBoard_GameIsEnd;
+
+            // Gọi event khi lượt chơicủa người chơi được đánh dấu
+            ChessBoard.PlayerMarked += ChessBoard_PlayerMarked;
+
+            // Thêm dữ liệu vào process bar
+            processBar.Step = Constant.COUNT_DOWN_STEP;
+            processBar.Maximum = Constant.COUNT_DOWN_TIME;
+            processBar.Value = 0;
+
+            timerCountDown.Interval = Constant.COUNT_DOWN_INTERVAL;
+            
             // Vẽ bàn cờ
             ChessBoard.DrawChessBoard();
 
-            settingControl = new SettingControl();
+        }
+
+        void EndGameAnnouncement()
+        {
+            timerCountDown.Stop();
+            chessBoardPanel.Enabled = false;
+            MessageBox.Show("Your game is end!!!", "Announcement", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ChessBoard_PlayerMarked(object sender, EventArgs e)
+        {
+            timerCountDown.Start();
+            processBar.Value = 0; // Reset thời gian
+        }
+
+        private void ChessBoard_GameIsEnd(object sender, EventArgs e)
+        {
+            EndGameAnnouncement();
+        }
+
+        private void timerCountDown_Tick(object sender, EventArgs e)
+        {
+            processBar.PerformStep();
+            
+            if (processBar.Value >= processBar.Maximum) 
+            {
+                EndGameAnnouncement();
+            }
+
         }
     }
 }
