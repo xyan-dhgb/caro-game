62dadaf27b5e5492b7c4d3c11c79646ffdfad064
Đinh Huỳnh Gia Bảo <100998772+andreafletcherdinh@users.noreply.github.com>
2024-05-29 22:51:54 +0700

    Add speed, interval and time for process bar

--- CARO_GAME/Constant.cs
+++ CARO_GAME/Constant.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,19 @@ namespace CARO_GAME
 {
     public class Constant
     {
+
+        // Kích thước ô cờ
         public static int CHESS_WIDTH = 30;
         public static int CHESS_HEIGTH = 30;
 
+        // Kích thước bàn cờ
         public static int CHESS_BOARD_WIDTH = 15;
         public static int CHESS_BOARD_HEIGHT = 20;
+
+        // Tốc độ
+        public static int COUNT_DOWN_STEP = 100; 
+        // Thời gian kết thúc là bao lâu?
+        public static int COUNT_DOWN_TIME = 10000;
+        public static int COUNT_DOWN_INTERVAL = 100;
     }
 }
